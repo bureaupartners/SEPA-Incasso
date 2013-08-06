caed40f4c1db30cf2563f27c13ee40e69bade7e4
Rein Schaap <info@reinschaap.nl>
2013-08-06 14:53:39 +0200

    Update sepa.class.php
    Class is wat uitgebreid:
    CollectionDestination is uitgebreid met incassant ID (creditor).
    Collection is uitgebreid met mandateID en dat is de unieke identificatie die bekend moet zijn bij de bank, de incassant (bedrijf) en de geïncasseerde (consument). Tevens is de EndToEndId tag toegevoegd en voor het gemak (dat mag van de banken) wordt ook daarvoor het MandateID ingevuld.

--- sepa.class.php
+++ sepa.class.php
@@ -1,8 +1,8 @@
 <?php
 /****
 	*	@name		SEPA Incasso class
-	*	@version	1.0 (08-07-2013)
-	*	@author		Mark Hameetman <mark@bureaupartners.nl>
+	*	@version	1.1 (08-07-2013)
+	*	@author		Mark Hameetman <mark@bureaupartners.nl> / Rein Schaap <info@reinschaap.nl>
 	*	@copyright	2013 BureauPartners B.V.
 	*	@link		https://www.bureaupartners.nl/2013/07/sepa-incasso-open-source-php-class/
 */
@@ -21,11 +21,12 @@ class SEPA {
 		$this->setDate('+1 day');
 	}
 	
-	public function setCollectionDestination($sName, $sIBAN, $sBIC){
+	public function setCollectionDestination($sName, $sIBAN, $sBIC, $sCREDITOR){
 		$this->aCollectionDestination	=	array(
-											'name'	=>	$sName,
-											'iban'	=>	$sIBAN,
-											'bic'	=>	$sBIC
+											'name'			=>	$sName,
+											'iban'			=>	$sIBAN,
+											'bic'			=>	$sBIC,
+											'creditorid'		=>	$sCREDITOR
 										);
 	}
 	
@@ -33,26 +34,28 @@ class SEPA {
 		$this->iDate = strtotime($sDate);
 	}
 	
-	public function addCollection($sName, $sIBAN, $sBIC, $iAmount, $sDescription, $sSignedDate){
+	public function addCollection($sName, $sIBAN, $sBIC, $iAmount, $sDescription, $sSignedDate, $sMandateID){
 		$iAmount = number_format($iAmount, 2, '.', '');
 		$this->iSum += $iAmount;
 		$this->aCollection[] = array(
-										'name'		=>	$sName,
-										'iban'		=>	$sIBAN,
-										'bic'		=>	$sBIC,
-										'amount'	=> 	$iAmount,
-										'descr'		=>	$sDescription,
-										'signDate'	=>	strtotime($sSignedDate)
+										'name'			=>	$sName,
+										'iban'			=>	$sIBAN,
+										'bic'			=>	$sBIC,
+										'amount'		=> 	$iAmount,
+										'descr'			=>	$sDescription,
+										'mandateid'		=>	$sMandateID,
+										'endtoendid'		=>	$sMandateID,
+										'signDate'		=>	strtotime($sSignedDate)
 							);
 	}
 	
 	private function addGroupHeader(){
-		$this->sXML .= '<GrpHdr><MsgId>'.time().'</MsgId><CreDtTm>'.date(DATE_ATOM).'</CreDtTm><NbOfTxs>'.count($this->aCollection).'</NbOfTxs><CtrlSum>'.number_format($this->iSum, 2, '.', '').'</CtrlSum><InitgPty><Nm>'.$this->aCollectionDestination['name'].'</Nm></InitgPty></GrpHdr><PmtInf><PmtInfId>'.time().'</PmtInfId><PmtMtd>DD</PmtMtd><NbOfTxs>'.count($this->aCollection).'</NbOfTxs><CtrlSum>'.number_format($this->iSum, 2, '.', '').'</CtrlSum><PmtTpInf><SvcLvl><Cd>SEPA</Cd></SvcLvl><LclInstrm><Cd>CORE</Cd></LclInstrm><SeqTp>FRST</SeqTp></PmtTpInf><ReqdColltnDt>'.date('Y-m-d', $this->iDate).'</ReqdColltnDt><Cdtr><Nm>'.$this->aCollectionDestination['name'].'</Nm></Cdtr><CdtrAcct><Id><IBAN>'.$this->aCollectionDestination['iban'].'</IBAN></Id></CdtrAcct><CdtrAgt><FinInstnId><BIC>'.$this->aCollectionDestination['bic'].'</BIC></FinInstnId></CdtrAgt><ChrgBr>SLEV</ChrgBr><CdtrSchmeId><Id><PrvtId><Othr><Id>'.$this->aCollectionDestination['iban'].'</Id><SchmeNm><Prtry>SEPA</Prtry></SchmeNm></Othr></PrvtId></Id></CdtrSchmeId>';
+		$this->sXML .= '<GrpHdr><MsgId>'.time().'</MsgId><CreDtTm>'.date(DATE_ATOM).'</CreDtTm><BtchBookg>true</BtchBookg><NbOfTxs>'.count($this->aCollection).'</NbOfTxs><CtrlSum>'.number_format($this->iSum, 2, '.', '').'</CtrlSum><InitgPty><Nm>'.$this->aCollectionDestination['name'].'</Nm></InitgPty></GrpHdr><PmtInf><PmtInfId>'.time().'</PmtInfId><PmtMtd>DD</PmtMtd><NbOfTxs>'.count($this->aCollection).'</NbOfTxs><CtrlSum>'.number_format($this->iSum, 2, '.', '').'</CtrlSum><PmtTpInf><SvcLvl><Cd>SEPA</Cd></SvcLvl><LclInstrm><Cd>CORE</Cd></LclInstrm><SeqTp>FRST</SeqTp></PmtTpInf><ReqdColltnDt>'.date('Y-m-d', $this->iDate).'</ReqdColltnDt><Cdtr><Nm>'.$this->aCollectionDestination['name'].'</Nm></Cdtr><CdtrAcct><Id><IBAN>'.$this->aCollectionDestination['iban'].'</IBAN></Id></CdtrAcct><CdtrAgt><FinInstnId><BIC>'.$this->aCollectionDestination['bic'].'</BIC></FinInstnId></CdtrAgt><ChrgBr>SLEV</ChrgBr><CdtrSchmeId><Id><PrvtId><Othr><Id>'.$this->aCollectionDestination['creditorid'].'</Id><SchmeNm><Prtry>SEPA</Prtry></SchmeNm></Othr></PrvtId></Id></CdtrSchmeId>';
 	}
 	
 	private function addCollections(){
-		foreach($this->aCollection AS $aCollect){
-			$this->sXML .=	'<DrctDbtTxInf><PmtId><EndToEndId>'.$aCollect['descr'].'</EndToEndId></PmtId><InstdAmt Ccy="'.$this->iCurrency.'">'.$aCollect['amount'].'</InstdAmt><DrctDbtTx><MndtRltdInf><MndtId>'.$aCollect['descr'].'</MndtId><DtOfSgntr>'.date('Y-m-d', $aCollect['signDate']).'</DtOfSgntr></MndtRltdInf></DrctDbtTx><DbtrAgt><FinInstnId><BIC>'.$aCollect['bic'].'</BIC></FinInstnId></DbtrAgt><Dbtr><Nm>'.$aCollect['name'].'</Nm></Dbtr><DbtrAcct><Id><IBAN>'.$aCollect['iban'].'</IBAN></Id></DbtrAcct><RmtInf><Ustrd>'.$aCollect['descr'].'</Ustrd></RmtInf></DrctDbtTxInf>';
+		foreach($this->aCollection as $aCollect){
+			$this->sXML .=	'<DrctDbtTxInf><PmtId><EndToEndId>'.$aCollect['endtoendid'].'</EndToEndId></PmtId><InstdAmt Ccy="'.$this->iCurrency.'">'.$aCollect['amount'].'</InstdAmt><DrctDbtTx><MndtRltdInf><MndtId>'.$aCollect['mandateid'].'</MndtId><DtOfSgntr>'.date('Y-m-d', $aCollect['signDate']).'</DtOfSgntr></MndtRltdInf></DrctDbtTx><DbtrAgt><FinInstnId><BIC>'.$aCollect['bic'].'</BIC></FinInstnId></DbtrAgt><Dbtr><Nm>'.$aCollect['name'].'</Nm></Dbtr><DbtrAcct><Id><IBAN>'.$aCollect['iban'].'</IBAN></Id></DbtrAcct><RmtInf><Ustrd>'.$aCollect['descr'].'</Ustrd></RmtInf></DrctDbtTxInf>';
 		}
 	}
 	
